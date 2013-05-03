ef6f8c6b8ed2f814cdfa47e36871a14365e054d4
Maciej Pasternacki <maciej@pasternacki.net>
2013-05-03 12:22:10 +0200

    Add comments

--- lib/nanodoc/pygments.rb
+++ lib/nanodoc/pygments.rb
@@ -7,6 +7,10 @@ module Nanodoc
   class Pygments < ::Nanoc::Store
     PYGMENTS2JSON = Pathname.new(__FILE__).dirname.join('pygments2json.py')
 
+    # initialize(item)
+    # ----------------
+    # `item` can be a `Nanoc::Item`, a `Pathname`, or a string with
+    # path to file.
     def initialize(item)
       item = Pathname.new(item) if item.is_a?(String)
       if item.is_a?(Pathname)
@@ -22,6 +26,12 @@ module Nanodoc
       super("tmp/pygments#{item.identifier}parsed", 1)
     end
 
+    # parsed()
+    # --------
+    # Returns an array parsed by Pygments. Each of the elements is an
+    # array of form: `[ string, token_type, css_class ]` (`css_class`
+    # may be omitted; in this case, text shouldn't be formatted with
+    # any class).
     def parsed
       load
       unless @parsed
